19b25e36233dba0fe926859bca2e784069e06c2d
Nick Palladino <np398@cornell.edu>
2020-05-11 15:03:17 -0400

    Added create method tests

---
+++ brapi-client/src/test/java/org/brapi/client/v2/modules/phenotype/MethodsAPIUnitTests.java
@@ -0,0 +1,42 @@
+package org.brapi.client.v2.modules.phenotype;
+
+import lombok.SneakyThrows;
+import org.brapi.client.v2.BrAPIClient;
+import org.brapi.client.v2.ResponseHandlerFunction;
+import org.brapi.client.v2.model.BrAPIRequest;
+import org.brapi.v2.phenotyping.model.BrApiMethod;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class MethodsAPIUnitTests {
+
+    MethodsAPI methodsAPI;
+    @Mock
+    BrAPIClient brAPIClient;
+
+    @BeforeAll
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        methodsAPI = new MethodsAPI(brAPIClient);
+    }
+
+    @Test
+    @SneakyThrows
+    void createTraitsEmptyBody() {
+        when(brAPIClient.execute(any(BrAPIRequest.class), any(ResponseHandlerFunction.class)))
+                .thenReturn(Optional.empty());
+        Optional<BrApiMethod> brApiMethod = methodsAPI.createMethod(new BrApiMethod());
+
+        assertEquals(false, brApiMethod.isPresent(), "Empty optional was not returned.");
+    }
+}
