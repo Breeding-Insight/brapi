e16ce866a7c1431a189fae9567d3d38452f86513
Chris T <ct447@cornell.edu>
2020-06-03 12:17:13 -0400

    fix external reference data type

--- brapi-core-model/src/main/java/org/brapi/v2/core/model/BrApiExternalReference.java
+++ brapi-core-model/src/main/java/org/brapi/v2/core/model/BrApiExternalReference.java
@@ -12,7 +12,7 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 @NoArgsConstructor
 public class BrApiExternalReference {
-  private Object referenceID;
+  private String referenceID;
 
   private String referenceSource;
 
