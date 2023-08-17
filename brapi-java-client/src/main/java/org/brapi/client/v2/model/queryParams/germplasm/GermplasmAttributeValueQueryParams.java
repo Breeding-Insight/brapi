49fe7ca6f56a6dbdfbd7c4558d90480ed0881fad
Peter Selby <ps664@cornell.edu>
2023-08-17 13:22:18 -0400

    fixes issues #75, #76, #77, #78, #139, #140. Dependant on #199

--- brapi-java-client/src/main/java/org/brapi/client/v2/model/queryParams/germplasm/GermplasmAttributeValueQueryParams.java
+++ brapi-java-client/src/main/java/org/brapi/client/v2/model/queryParams/germplasm/GermplasmAttributeValueQueryParams.java
@@ -26,30 +26,33 @@ import lombok.experimental.SuperBuilder;
 
 import org.brapi.client.v2.model.queryParams.core.BrAPIQueryParams;
 
-
 @Getter
 @Setter
 @SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
-@Accessors(fluent=true)
+@Accessors(fluent = true)
 public class GermplasmAttributeValueQueryParams extends BrAPIQueryParams {
 
-    private String attributeValueDbId;
-    private String attributeDbId;
-    private String attributeName;
-    private String germplasmDbId;
-    private String externalReferenceSource;
-    private String externalReferenceId;
-    @Deprecated
-    private String externalReferenceID;
-    
-    public String getExternalReferenceId() {
+	private String attributeValueDbId;
+	private String attributeDbId;
+	private String attributeName;
+	private String germplasmDbId;
+	private String externalReferenceSource;
+	private String externalReferenceId;
+	@Deprecated
+	private String externalReferenceID;
+	private String programDbId;
+	private String commonCropName;
+
+	public String getExternalReferenceId() {
 		return externalReferenceId;
 	}
-    public String externalReferenceId() {
+
+	public String externalReferenceId() {
 		return externalReferenceId;
 	}
+
 	public void setExternalReferenceId(String externalReferenceId) {
 		this.externalReferenceId = externalReferenceId;
 	}
@@ -58,10 +61,12 @@ public class GermplasmAttributeValueQueryParams extends BrAPIQueryParams {
 	public String getExternalReferenceID() {
 		return externalReferenceID;
 	}
+
 	@Deprecated
 	public String externalReferenceID() {
 		return externalReferenceID;
 	}
+
 	@Deprecated
 	public void setExternalReferenceID(String externalReferenceID) {
 		this.externalReferenceID = externalReferenceID;
