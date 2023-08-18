79fc2f7519636b1c5ed4d0a91e03346edb112810
Peter Selby <ps664@cornell.edu>
2023-08-18 15:05:24 -0400

    fixes #174, #175, #176, #177, #178, #179, #157, #158. Dependant on #199

--- brapi-java-client/src/main/java/org/brapi/client/v2/modules/core/ServerInfoApi.java
+++ brapi-java-client/src/main/java/org/brapi/client/v2/modules/core/ServerInfoApi.java
@@ -28,93 +28,128 @@ import org.brapi.v2.model.BrAPIWSMIMEDataTypes;
 import org.brapi.v2.model.core.response.BrAPIServerInfoResponse;
 
 public class ServerInfoApi {
-    private BrAPIClient apiClient;
+	private BrAPIClient apiClient;
 
-    public ServerInfoApi() {
-        this(Configuration.getDefaultApiClient());
-    }
+	public ServerInfoApi() {
+		this(Configuration.getDefaultApiClient());
+	}
 
-    public ServerInfoApi(BrAPIClient apiClient) {
-        this.apiClient = apiClient;
-    }
+	public ServerInfoApi(BrAPIClient apiClient) {
+		this.apiClient = apiClient;
+	}
 
-    public BrAPIClient getApiClient() {
-        return apiClient;
-    }
+	public BrAPIClient getApiClient() {
+		return apiClient;
+	}
 
-    public void setApiClient(BrAPIClient apiClient) {
-        this.apiClient = apiClient;
-    }
+	public void setApiClient(BrAPIClient apiClient) {
+		this.apiClient = apiClient;
+	}
 
-    /**
-     * Build call for serverinfoGet
-     * @param dataType The data format supported by the call. (optional)
+	/**
+	 * Build call for serverinfoGet
+	 * 
+	 * @param contentType The data format supported by the call. (optional)
+	 * @return Call to execute
+	 * @throws ApiException If fail to serialize the request body object
+	 */
+	private Call serverinfoGetCall(BrAPIWSMIMEDataTypes contentType) throws ApiException {
+		Object localVarPostBody = null;
 
+		// create path and map variables
+		String localVarPath = "/serverinfo";
 
+		Map<String, String> localVarQueryParams = new HashMap<>();
+		Map<String, String> localVarCollectionQueryParams = new HashMap<>();
+		if (contentType != null)
+			apiClient.prepQueryParameter(localVarQueryParams, "dataType", contentType);
 
-     * @return Call to execute
-     * @throws ApiException If fail to serialize the request body object
-     */
-    private Call serverinfoGetCall(BrAPIWSMIMEDataTypes dataType) throws ApiException {
-        Object localVarPostBody = null;
-        
-        // create path and map variables
-        String localVarPath = "/serverinfo";
+		Map<String, String> localVarHeaderParams = new HashMap<String, String>();
 
-        Map<String, String> localVarQueryParams = new HashMap<>();
-        Map<String, String> localVarCollectionQueryParams = new HashMap<>();
-        if (dataType != null)
-            apiClient.prepQueryParameter(localVarQueryParams, "dataType", dataType);
+		Map<String, Object> localVarFormParams = new HashMap<String, Object>();
 
-        Map<String, String> localVarHeaderParams = new HashMap<String, String>();
-        
-        
+		final String[] localVarAccepts = { "application/json" };
+		final String localVarAccept = apiClient.selectHeaderAccept(localVarAccepts);
+		if (localVarAccept != null)
+			localVarHeaderParams.put("Accept", localVarAccept);
 
-        Map<String, Object> localVarFormParams = new HashMap<String, Object>();
+		final String[] localVarContentTypes = {
 
-        final String[] localVarAccepts = {
-            "application/json"
-        };
-        final String localVarAccept = apiClient.selectHeaderAccept(localVarAccepts);
-        if (localVarAccept != null) localVarHeaderParams.put("Accept", localVarAccept);
+		};
+		final String localVarContentType = apiClient.selectHeaderContentType(localVarContentTypes);
+		localVarHeaderParams.put("Content-Type", localVarContentType);
 
-        final String[] localVarContentTypes = {
-            
-        };
-        final String localVarContentType = apiClient.selectHeaderContentType(localVarContentTypes);
-        localVarHeaderParams.put("Content-Type", localVarContentType);
+		String[] localVarAuthNames = new String[] { "AuthorizationToken" };
+		return apiClient.buildCall(localVarPath, "GET", localVarQueryParams, localVarCollectionQueryParams,
+				localVarPostBody, localVarHeaderParams, localVarFormParams, localVarAuthNames);
+	}
 
-        String[] localVarAuthNames = new String[] { "AuthorizationToken" };
-        return apiClient.buildCall(localVarPath, "GET", localVarQueryParams, localVarCollectionQueryParams, localVarPostBody, localVarHeaderParams, localVarFormParams, localVarAuthNames);
-    }
+	/**
+	 * Get the list of implemented Calls Implementation Notes Having a consistent
+	 * structure for the path string of each call is very important for teams to be
+	 * able to connect and find errors. Read more on Github. Here are the rules for
+	 * the path of each call that should be returned Every word in the call path
+	 * should match the documentation exactly, both in spelling and capitalization.
+	 * Note that path strings are all lower case, but path parameters are camel
+	 * case. Each path should start relative to \\\&quot;/\\\&quot; and therefore
+	 * should not include \\\&quot;/\\\&quot; No leading or trailing slashes
+	 * (\\\&quot;/\\\&quot;) Path parameters are wrapped in curly braces
+	 * (\\\&quot;{}\\\&quot;). The name of the path parameter should be spelled
+	 * exactly as it is specified in the documentation. Examples GOOD
+	 * \&quot;call\&quot;: \&quot;germplasm/{germplasmDbId}/pedigree\&quot; BAD
+	 * \&quot;call\&quot;: \&quot;germplasm/{id}/pedigree\&quot; BAD
+	 * \&quot;call\&quot;: \&quot;germplasm/{germplasmDBid}/pedigree\&quot; BAD
+	 * \&quot;call\&quot;: \&quot;brapi/v2/germplasm/{germplasmDbId}/pedigree\&quot;
+	 * BAD \&quot;call\&quot;: \&quot;/germplasm/{germplasmDbId}/pedigree/\&quot;
+	 * BAD \&quot;call\&quot;:
+	 * \&quot;germplasm/&lt;germplasmDbId&gt;/pedigree\&quot;
+	 * 
+	 * @param contentType The data format supported by the call. (optional)
+	 * 
+	 * @return ApiResponse&lt;ServerInfoResponse&gt;
+	 * @throws ApiException If fail to call the API, e.g. server error or cannot
+	 *                      deserialize the response body
+	 */
+	public ApiResponse<BrAPIServerInfoResponse> serverinfoGet(BrAPIWSMIMEDataTypes contentType) throws ApiException {
+		Call call = serverinfoGetCall(contentType);
+		Type localVarReturnType = new TypeToken<BrAPIServerInfoResponse>() {
+		}.getType();
+		return apiClient.execute(call, localVarReturnType);
+	}
 
-    /**
-     * Get the list of implemented Calls
-     * Implementation Notes  Having a consistent structure for the path string of each call is very  important for teams to be able to connect and find errors. Read more on Github.  Here are the rules for the path of each call that should be returned  Every word in the call path should match the documentation exactly, both in  spelling and capitalization. Note that path strings are all lower case, but  path parameters are camel case.  Each path should start relative to \\\&quot;/\\\&quot; and therefore should not include \\\&quot;/\\\&quot;  No leading or trailing slashes (\\\&quot;/\\\&quot;)   Path parameters are wrapped in curly braces (\\\&quot;{}\\\&quot;). The name of the path parameter  should be spelled exactly as it is specified in the documentation.  Examples   GOOD   \&quot;call\&quot;: \&quot;germplasm/{germplasmDbId}/pedigree\&quot;   BAD    \&quot;call\&quot;: \&quot;germplasm/{id}/pedigree\&quot;  BAD    \&quot;call\&quot;: \&quot;germplasm/{germplasmDBid}/pedigree\&quot;   BAD    \&quot;call\&quot;: \&quot;brapi/v2/germplasm/{germplasmDbId}/pedigree\&quot;   BAD    \&quot;call\&quot;: \&quot;/germplasm/{germplasmDbId}/pedigree/\&quot;   BAD    \&quot;call\&quot;: \&quot;germplasm/&lt;germplasmDbId&gt;/pedigree\&quot;
-     * @param dataType The data format supported by the call. (optional)
-
-     * @return ApiResponse&lt;ServerInfoResponse&gt;
-     * @throws ApiException If fail to call the API, e.g. server error or cannot deserialize the response body
-     */
-    public ApiResponse<BrAPIServerInfoResponse> serverinfoGet(BrAPIWSMIMEDataTypes dataType) throws ApiException {
-        Call call = serverinfoGetCall(dataType);
-        Type localVarReturnType = new TypeToken<BrAPIServerInfoResponse>(){}.getType();
-        return apiClient.execute(call, localVarReturnType);
-    }
-
-    /**
-     * Get the list of implemented Calls (asynchronously)
-     * Implementation Notes  Having a consistent structure for the path string of each call is very  important for teams to be able to connect and find errors. Read more on Github.  Here are the rules for the path of each call that should be returned  Every word in the call path should match the documentation exactly, both in  spelling and capitalization. Note that path strings are all lower case, but  path parameters are camel case.  Each path should start relative to \\\&quot;/\\\&quot; and therefore should not include \\\&quot;/\\\&quot;  No leading or trailing slashes (\\\&quot;/\\\&quot;)   Path parameters are wrapped in curly braces (\\\&quot;{}\\\&quot;). The name of the path parameter  should be spelled exactly as it is specified in the documentation.  Examples   GOOD   \&quot;call\&quot;: \&quot;germplasm/{germplasmDbId}/pedigree\&quot;   BAD    \&quot;call\&quot;: \&quot;germplasm/{id}/pedigree\&quot;  BAD    \&quot;call\&quot;: \&quot;germplasm/{germplasmDBid}/pedigree\&quot;   BAD    \&quot;call\&quot;: \&quot;brapi/v2/germplasm/{germplasmDbId}/pedigree\&quot;   BAD    \&quot;call\&quot;: \&quot;/germplasm/{germplasmDbId}/pedigree/\&quot;   BAD    \&quot;call\&quot;: \&quot;germplasm/&lt;germplasmDbId&gt;/pedigree\&quot;
-     * @param dataType The data format supported by the call. (optional)
-
-     * @param callback The callback to be executed when the API call finishes
-     * @return The request call
-     * @throws ApiException If fail to process the API call, e.g. serializing the request body object
-     */
-    public Call serverinfoGetAsync(BrAPIWSMIMEDataTypes dataType, final ApiCallback<BrAPIServerInfoResponse> callback) throws ApiException {
-        Call call = serverinfoGetCall(dataType);
-        Type localVarReturnType = new TypeToken<BrAPIServerInfoResponse>(){}.getType();
-        apiClient.executeAsync(call, localVarReturnType, callback);
-        return call;
-    }
+	/**
+	 * Get the list of implemented Calls (asynchronously) Implementation Notes
+	 * Having a consistent structure for the path string of each call is very
+	 * important for teams to be able to connect and find errors. Read more on
+	 * Github. Here are the rules for the path of each call that should be returned
+	 * Every word in the call path should match the documentation exactly, both in
+	 * spelling and capitalization. Note that path strings are all lower case, but
+	 * path parameters are camel case. Each path should start relative to
+	 * \\\&quot;/\\\&quot; and therefore should not include \\\&quot;/\\\&quot; No
+	 * leading or trailing slashes (\\\&quot;/\\\&quot;) Path parameters are wrapped
+	 * in curly braces (\\\&quot;{}\\\&quot;). The name of the path parameter should
+	 * be spelled exactly as it is specified in the documentation. Examples GOOD
+	 * \&quot;call\&quot;: \&quot;germplasm/{germplasmDbId}/pedigree\&quot; BAD
+	 * \&quot;call\&quot;: \&quot;germplasm/{id}/pedigree\&quot; BAD
+	 * \&quot;call\&quot;: \&quot;germplasm/{germplasmDBid}/pedigree\&quot; BAD
+	 * \&quot;call\&quot;: \&quot;brapi/v2/germplasm/{germplasmDbId}/pedigree\&quot;
+	 * BAD \&quot;call\&quot;: \&quot;/germplasm/{germplasmDbId}/pedigree/\&quot;
+	 * BAD \&quot;call\&quot;:
+	 * \&quot;germplasm/&lt;germplasmDbId&gt;/pedigree\&quot;
+	 * 
+	 * @param contentType The data format supported by the call. (optional)
+	 * 
+	 * @param callback    The callback to be executed when the API call finishes
+	 * @return The request call
+	 * @throws ApiException If fail to process the API call, e.g. serializing the
+	 *                      request body object
+	 */
+	public Call serverinfoGetAsync(BrAPIWSMIMEDataTypes contentType,
+			final ApiCallback<BrAPIServerInfoResponse> callback) throws ApiException {
+		Call call = serverinfoGetCall(contentType);
+		Type localVarReturnType = new TypeToken<BrAPIServerInfoResponse>() {
+		}.getType();
+		apiClient.executeAsync(call, localVarReturnType, callback);
+		return call;
+	}
 }
--- brapi-java-model/src/main/java/org/brapi/v2/model/core/BrAPIService.java
+++ brapi-java-model/src/main/java/org/brapi/v2/model/core/BrAPIService.java
@@ -10,98 +10,53 @@ import com.fasterxml.jackson.annotation.JsonValue;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.validation.Valid;
-
+import org.brapi.v2.model.BrAPIEnum;
 import org.brapi.v2.model.BrAPIWSMIMEDataTypes;
 
 /**
  * Service
  */
 
-
 public class BrAPIService {
-	/**
-	 * Gets or Sets methods
-	 */
-	public enum MethodsEnum {
-		GET("GET"),
 
-		POST("POST"),
+	@JsonProperty("contentTypes")
+	private List<BrAPIWSMIMEDataTypes> contentTypes = null;
 
-		PUT("PUT"),
-
-		DELETE("DELETE");
-
-		@JsonCreator
-		public static MethodsEnum fromValue(String text) {
-			for (MethodsEnum b : MethodsEnum.values()) {
-				if (String.valueOf(b.value).equals(text)) {
-					return b;
-				}
-			}
-			return null;
-		}
-
-		private String value;
-
-		MethodsEnum(String value) {
-			this.value = value;
-		}
-
-		@Override
-		@JsonValue
-		public String toString() {
-			return String.valueOf(value);
-		}
-	}
-
-	/**
-	 * Gets or Sets versions
-	 */
-	public enum VersionsEnum {
-		_0("2.0"),
-
-		_1("2.1"),
-
-		_2("2.2");
-
-		@JsonCreator
-		public static VersionsEnum fromValue(String text) {
-			for (VersionsEnum b : VersionsEnum.values()) {
-				if (String.valueOf(b.value).equals(text)) {
-					return b;
-				}
-			}
-			return null;
-		}
-
-		private String value;
-
-		VersionsEnum(String value) {
-			this.value = value;
-		}
-
-		@Override
-		@JsonValue
-		public String toString() {
-			return String.valueOf(value);
-		}
-	}
-
+	@Deprecated
 	@JsonProperty("dataTypes")
-	@Valid
 	private List<BrAPIWSMIMEDataTypes> dataTypes = null;
 
 	@JsonProperty("methods")
-	@Valid
 	private List<MethodsEnum> methods = new ArrayList<MethodsEnum>();
 
 	@JsonProperty("service")
 	private String service = null;
+
 	@JsonProperty("versions")
-	@Valid
 	private List<VersionsEnum> versions = new ArrayList<VersionsEnum>();
 
+	public BrAPIService addContentTypesItem(BrAPIWSMIMEDataTypes contentTypesItem) {
+		if (this.contentTypes == null) {
+			this.contentTypes = new ArrayList<BrAPIWSMIMEDataTypes>();
+		}
+		this.contentTypes.add(contentTypesItem);
+		return this;
+	}
+
+	public BrAPIService contentTypes(List<BrAPIWSMIMEDataTypes> contentTypes) {
+		this.contentTypes = contentTypes;
+		return this;
+	}
+
+	public List<BrAPIWSMIMEDataTypes> getContentTypes() {
+		return contentTypes;
+	}
+
+	public void setContentTypes(List<BrAPIWSMIMEDataTypes> contentTypes) {
+		this.contentTypes = contentTypes;
+	}
+
+	@Deprecated
 	public BrAPIService addDataTypesItem(BrAPIWSMIMEDataTypes dataTypesItem) {
 		if (this.dataTypes == null) {
 			this.dataTypes = new ArrayList<BrAPIWSMIMEDataTypes>();
@@ -110,9 +65,51 @@ public class BrAPIService {
 		return this;
 	}
 
+	@Deprecated
+	public BrAPIService dataTypes(List<BrAPIWSMIMEDataTypes> dataTypes) {
+		this.dataTypes = dataTypes;
+		return this;
+	}
+
+	@Deprecated
+	public List<BrAPIWSMIMEDataTypes> getDataTypes() {
+		return dataTypes;
+	}
+
+	@Deprecated
+	public void setDataTypes(List<BrAPIWSMIMEDataTypes> dataTypes) {
+		this.dataTypes = dataTypes;
+	}
+
 	public BrAPIService addMethodsItem(MethodsEnum methodsItem) {
 		this.methods.add(methodsItem);
 		return this;
+	}
+
+	public List<MethodsEnum> getMethods() {
+		return methods;
+	}
+
+	public BrAPIService methods(List<MethodsEnum> methods) {
+		this.methods = methods;
+		return this;
+	}
+
+	public void setMethods(List<MethodsEnum> methods) {
+		this.methods = methods;
+	}
+
+	public String getService() {
+		return service;
+	}
+
+	public BrAPIService service(String service) {
+		this.service = service;
+		return this;
+	}
+
+	public void setService(String service) {
+		this.service = service;
 	}
 
 	public BrAPIService addVersionsItem(VersionsEnum versionsItem) {
@@ -120,9 +117,12 @@ public class BrAPIService {
 		return this;
 	}
 
-	public BrAPIService dataTypes(List<BrAPIWSMIMEDataTypes> dataTypes) {
-		this.dataTypes = dataTypes;
-		return this;
+	public List<VersionsEnum> getVersions() {
+		return versions;
+	}
+
+	public void setVersions(List<VersionsEnum> versions) {
+		this.versions = versions;
 	}
 
 	@Override
@@ -134,86 +134,15 @@ public class BrAPIService {
 			return false;
 		}
 		BrAPIService service = (BrAPIService) o;
-		return Objects.equals(this.dataTypes, service.dataTypes) && Objects.equals(this.methods, service.methods)
-				&& Objects.equals(this.service, service.service) && Objects.equals(this.versions, service.versions);
-	}
-
-	/**
-	 * The possible data formats returned by the available call
-	 * 
-	 * @return dataTypes
-	 **/
-	
-	@Valid
-	public List<BrAPIWSMIMEDataTypes> getDataTypes() {
-		return dataTypes;
-	}
-
-	/**
-	 * The possible HTTP Methods to be used with the available call
-	 * 
-	 * @return methods
-	 **/
-	
-	
-
-	public List<MethodsEnum> getMethods() {
-		return methods;
-	}
-
-	/**
-	 * The name of the available call as recorded in the documentation
-	 * 
-	 * @return service
-	 **/
-	
-	
-
-	public String getService() {
-		return service;
-	}
-
-	/**
-	 * The supported versions of a particular call
-	 * 
-	 * @return versions
-	 **/
-	
-	
-
-	public List<VersionsEnum> getVersions() {
-		return versions;
+		return Objects.equals(this.dataTypes, service.dataTypes)
+				&& Objects.equals(this.contentTypes, service.contentTypes)
+				&& Objects.equals(this.methods, service.methods) && Objects.equals(this.service, service.service)
+				&& Objects.equals(this.versions, service.versions);
 	}
 
 	@Override
 	public int hashCode() {
 		return Objects.hash(dataTypes, methods, service, versions);
-	}
-
-	public BrAPIService methods(List<MethodsEnum> methods) {
-		this.methods = methods;
-		return this;
-	}
-
-	public BrAPIService service(String service) {
-		this.service = service;
-		return this;
-	}
-
-	public void setDataTypes(List<BrAPIWSMIMEDataTypes> dataTypes) {
-		this.dataTypes = dataTypes;
-	}
-
-	public void setMethods(List<MethodsEnum> methods) {
-		this.methods = methods;
-	}
-
-	public void setService(String service) {
-		this.service = service;
-	}
-
-	public void setVersions(List<VersionsEnum> versions) {
-		this.versions = versions;
 	}
 
 	/**
@@ -243,5 +172,81 @@ public class BrAPIService {
 	public BrAPIService versions(List<VersionsEnum> versions) {
 		this.versions = versions;
 		return this;
+	}
+
+	public enum MethodsEnum implements BrAPIEnum {
+		GET("GET"),
+
+		POST("POST"),
+
+		PUT("PUT"),
+
+		DELETE("DELETE");
+
+		@JsonCreator
+		public static MethodsEnum fromValue(String text) {
+			for (MethodsEnum b : MethodsEnum.values()) {
+				if (String.valueOf(b.value).equals(text)) {
+					return b;
+				}
+			}
+			return null;
+		}
+
+		private String value;
+
+		MethodsEnum(String value) {
+			this.value = value;
+		}
+
+		@Override
+		@JsonValue
+		public String toString() {
+			return String.valueOf(value);
+		}
+
+		@Override
+		public String getBrapiValue() {
+			// TODO Auto-generated method stub
+			return value;
+		}
+	}
+
+	/**
+	 * Gets or Sets versions
+	 */
+	public enum VersionsEnum implements BrAPIEnum {
+		_0("2.0"),
+
+		_1("2.1"),
+
+		_2("2.2");
+
+		@JsonCreator
+		public static VersionsEnum fromValue(String text) {
+			for (VersionsEnum b : VersionsEnum.values()) {
+				if (String.valueOf(b.value).equals(text)) {
+					return b;
+				}
+			}
+			return null;
+		}
+
+		private String value;
+
+		VersionsEnum(String value) {
+			this.value = value;
+		}
+
+		@Override
+		@JsonValue
+		public String toString() {
+			return String.valueOf(value);
+		}
+
+		@Override
+		public String getBrapiValue() {
+			return value;
+		}
 	}
 }
