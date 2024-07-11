b50314c480956f19c4bbf3abbfc8107e1b2f6c66
mlm483 <128052931+mlm483@users.noreply.github.com>
2024-07-11 16:39:07 -0400

    [BI-2208] - implemented hashCode, equals and toString for BrAPIGermplasmStorageTypes

--- brapi-java-model/src/main/java/org/brapi/v2/model/germ/BrAPIGermplasmStorageTypes.java
+++ brapi-java-model/src/main/java/org/brapi/v2/model/germ/BrAPIGermplasmStorageTypes.java
@@ -1,5 +1,7 @@
 package org.brapi.v2.model.germ;
 
+import java.util.Objects;
+
 public class BrAPIGermplasmStorageTypes {
 	private BrAPIGermplasmStorageTypesEnum code;
 	private String description;
@@ -25,4 +27,31 @@ public class BrAPIGermplasmStorageTypes {
 		return description;
 	}
 
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		} else if (o != null && this.getClass() == o.getClass()) {
+			BrAPIGermplasmStorageTypes germplasmStorageType = (BrAPIGermplasmStorageTypes)o;
+			return Objects.equals(this.code, germplasmStorageType.code) && Objects.equals(this.description, germplasmStorageType.description);
+		} else {
+			return false;
+		}
+	}
+
+	public int hashCode() {
+		return Objects.hash(this.code, this.description);
+	}
+
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("class BrAPIGermplasmStorageTypes {\n");
+		sb.append("    code: ").append(this.toIndentedString(this.code)).append("\n");
+		sb.append("    description: ").append(this.toIndentedString(this.description)).append("\n");
+		sb.append("}");
+		return sb.toString();
+	}
+
+	private String toIndentedString(Object o) {
+		return o == null ? "null" : o.toString().replace("\n", "\n    ");
+	}
 }
